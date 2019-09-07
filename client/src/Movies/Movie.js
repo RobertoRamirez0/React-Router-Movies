311c1a0fc4a89d98785cacd4ccc313bbe5bb3ec8
RobertoRamirez0 <ramirez.robertomu22@gmail.com>
2019-09-07 14:37:51 -0400

    step 1 done

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -2,10 +2,9 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Movie = (props) => {
-  const [movie, setMovie] = useState({});
- 
+  const [movie, setMovie] = useState();
   useEffect(() => {
-    const id = 1;
+    const id = props.match.params.id;
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
@@ -30,7 +29,7 @@ const Movie = (props) => {
     return <div>Loading movie information...</div>;
   }
 
-  const { title, director, metascore, stars } = movie;
+  const { title, director, metascore, stars,} = movie;
   return (
     <div className="save-wrapper">
       <div className="movie-card">
